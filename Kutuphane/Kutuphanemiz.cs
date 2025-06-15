f88e29e6beb5b30322d53a7ac8bab4ea1ac5fdb8
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-06-16 00:35:37 +0300

    Add project files.

---
+++ Kutuphane/Kutuphanemiz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane
+{
+    internal class Kutuphanemiz
+    {
+        public Kutuphanemiz()
+        {
+            Console.WriteLine("Default constructor oluşturuluyor.");
+        }
+        public Kutuphanemiz(string kitapadi, string yazaradi, string yazarsoyadi, int sayfasayisi, string yayinevi, DateTime kayittarihi)
+        {
+            KitapAdi = kitapadi;
+            YazarAdi = yazaradi;
+            YazarSoyadi = yazarsoyadi;
+            SayfaSayisi = sayfasayisi;
+            YayinEvi = yayinevi;
+            KayitTarihi = kayittarihi;
+        }
+        public string KitapAdi { get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string YayinEvi { get; set; }
+        public DateTime KayitTarihi { get; set; }
+
+        public void KutuphaneBilgisi()
+        {
+            Console.WriteLine(YazarAdi + "'in " + SayfaSayisi + " sayfalık, " + YayinEvi + " yayınevinden çıkan " + KitapAdi + " kitabını oluşturunuz." + " Oluşturulma Tarihi: " + KayitTarihi);
+        }
+    }
+}
---
+++ Kutuphane/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+using Kutuphane;
+
+Kutuphanemiz kitap1 = new Kutuphanemiz()
+{
+    KitapAdi = "Birinci İsimli",
+    YazarAdi = "Ahmet",
+    YazarSoyadi = "Yılmaz",
+    SayfaSayisi = 320,
+    YayinEvi = "Alfa Yayıncılık",
+    KayitTarihi = DateTime.Now,
+};
+
+Kutuphanemiz kitap2 = new Kutuphanemiz()
+{
+    KitapAdi = "İkinci İsimli",
+    YazarAdi = "Ayşe",
+    YazarSoyadi = "Karan",
+    SayfaSayisi = 215,
+    YayinEvi = "Beta Yayıncılık",
+    KayitTarihi = DateTime.Now,
+};
+
+Kutuphanemiz kitap3 = new Kutuphanemiz()
+{
+    KitapAdi = "Üçüncü İsimli",
+    YazarAdi = "Mehmet",
+    YazarSoyadi = "Demir",
+    SayfaSayisi = 450,
+    YayinEvi = "Gamma Yayıncılık",
+    KayitTarihi = DateTime.Now,
+};
+
+Kutuphanemiz kitap4 = new Kutuphanemiz()
+{
+    KitapAdi = "Dördüncü İsimli",
+    YazarAdi = "Elif",
+    YazarSoyadi = "Şahin",
+    SayfaSayisi = 275,
+    YayinEvi = "Delta Yayıncılık",
+    KayitTarihi = DateTime.Now,
+};
+
+Kutuphanemiz kitap5 = new Kutuphanemiz()
+{
+    KitapAdi = "Beşinci İsimli",
+    YazarAdi = "Selin",
+    YazarSoyadi = "Yurt",
+    SayfaSayisi = 198,
+    YayinEvi = "Epsilon Yayıncılık",
+    KayitTarihi = DateTime.Now,
+};
+
+Console.WriteLine(kitap1.YazarAdi + "'in " + kitap1.SayfaSayisi + " sayfalık, " + kitap1.YayinEvi + " yayınevinden çıkan " + kitap1.KitapAdi + " kitabını oluşturunuz." + " Oluşturulma Tarihi: " + kitap1.KayitTarihi );
+
+Kutuphanemiz yeniKitap = new Kutuphanemiz("Altıncı Kitap","Fatih","Unal",398,"Yayınevi Bilgisi",DateTime.Now);
+
+yeniKitap.KutuphaneBilgisi();
